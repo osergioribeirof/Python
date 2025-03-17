62a397c7ad9fa3fb9c0360b6745f3be2a11a0a51
osergioribeirof <OSERGIORIBEIRO@ICLOUD.COM>
2025-03-17 20:03:25 -0300

    save

--- Mercado Financeiro/app.py
+++ Mercado Financeiro/app.py
@@ -1,15 +1,13 @@
 import streamlit as st
-import BCB_Dados_Econômicos_do_Brasil_  # Importa o código com o gráfico
+import BCB_Dados_Economicos_do_Brasil_  # Importa o código com o gráfico
 
 st.title("Gráfico do BCB - Volume Services")
 
 # Chama a função do outro arquivo
-figura = BCB_Dados_Econômicos_do_Brasil_.volumeservices()
+figura = BCB_Dados_Economicos_do_Brasil_.volumeservices()
 
 # Verifica se a função retornou um gráfico válido
 if figura:
     st.pyplot(figura)  # Exibe o gráfico no Streamlit
 else:
     st.error("Erro: volumeservices() não retornou um gráfico válido.")
-    
-    
