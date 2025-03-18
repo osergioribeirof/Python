1f5b133ea1e5fb262bc11f3c4c5f6bd7f32ce964
osergioribeirof <OSERGIORIBEIRO@ICLOUD.COM>
2025-03-17 23:32:59 -0300

    save

--- Mercado Financeiro App/app.py
+++ Mercado Financeiro App/app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.title("BCB - Volume Services")
+# Exemplo de gráfico
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.title('Sine Wave')
+
+# Exibir no Streamlit
+st.pyplot(plt)
 
