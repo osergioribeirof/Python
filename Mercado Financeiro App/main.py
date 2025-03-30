02fd734a25638d2b17545b323821eb5cce2a5c7b
osergioribeirof <OSERGIORIBEIRO@ICLOUD.COM>
2025-03-30 12:07:01 -0300

    save

--- Mercado Financeiro App/main.py
+++ Mercado Financeiro App/main.py
@@ -9,7 +9,6 @@ st.write(""" # BCB - DADOS ECONÔMICOS DO BRASIL """)
 
 import streamlit as st
 import pandas as pd
-import yfinance as yf
 import requests
 
 st.write("""# BCB - DADOS ECONÔMICOS DO BRASIL""")
@@ -29,11 +28,14 @@ if response.status_code == 200:
     VolumeServices['data'] = pd.to_datetime(VolumeServices['data'], format='%d/%m/%Y')
     VolumeServices.set_index('data', inplace=True)
     
+    # Ordenar os dados por data
+    VolumeServices.sort_index(inplace=True)
+    
     # Exibir as datas mais recentes no console
     print("Dados mais recentes:")
     print(VolumeServices.tail())  # Exibe as últimas 5 linhas do DataFrame
     
     # Criar o gráfico no Streamlit
-    st.line_chart(VolumeServices['valor'].tail())  # Exibe apenas a coluna 'valor'
+    st.line_chart(VolumeServices['valor'])  # Exibe a coluna 'valor' ordenada
 else:
     st.write(f"Erro ao acessar a API: {response.status_code}")
