e9e46921b320fa4154b18d7bfe58f0f0577913f7
osergioribeirof <OSERGIORIBEIRO@ICLOUD.COM>
2025-03-30 12:11:43 -0300

    save

--- Mercado Financeiro App/main.py
+++ Mercado Financeiro App/main.py
@@ -15,6 +15,15 @@ st.write("""# BCB - DADOS ECONÔMICOS DO BRASIL""")
 
 # VOLUME SERVICES
 
+import streamlit as st
+import pandas as pd
+import requests
+import plotly.express as px
+
+st.write("""# BCB - DADOS ECONÔMICOS DO BRASIL""")
+
+# VOLUME SERVICES
+
 # URL da API para o código 23982
 url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.23982/dados?formato=json"
 
@@ -35,7 +44,19 @@ if response.status_code == 200:
     print("Dados mais recentes:")
     print(VolumeServices.tail())  # Exibe as últimas 5 linhas do DataFrame
     
-    # Criar o gráfico no Streamlit
-    st.line_chart(VolumeServices['valor'])  # Exibe a coluna 'valor' ordenada
+    # Criar o gráfico com Plotly Express
+    fig = px.line(
+        VolumeServices,
+        x=VolumeServices.index,
+        y='valor',
+        title='Volume Services Monthly % var.'
+    )
+    
+    # Configurar o intervalo do eixo X e a cor da linha
+    fig.update_xaxes(range=['2010-01-01', str(VolumeServices.index.max().date())])
+    fig.update_traces(line_color='#00008B')  # '#00008B' é o código hexadecimal para azul escuro
+
+    # Mostrar o gráfico no Streamlit
+    st.plotly_chart(fig, use_container_width=True)
 else:
     st.write(f"Erro ao acessar a API: {response.status_code}")
